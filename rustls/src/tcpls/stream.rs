9f9da9ad274133819711f7bfeead6fca96a7d9a0
Romain Grimau <romain.grimau@hotmail.be>
2023-09-03 00:36:55 +0200

    replace hardcoded number with const

--- rustls/src/tcpls/stream.rs
+++ rustls/src/tcpls/stream.rs
@@ -37,13 +37,13 @@ impl TcplsStream {
         trace!("stream {} has a data frame", self.stream_id);
         let mut cursor: usize = new_data.len();
 
-        let stream_offset: u64 = conversion::slice_to_u64(&new_data[cursor - 8..cursor])
+        let stream_offset: u64 = conversion::slice_to_u64(&new_data[cursor - OFFSET_SIZE..cursor])
             .expect("Failed to convert bytes");
         cursor -= OFFSET_SIZE;
 
         self.rcv_offset = stream_offset;
 
-        let stream_len: u16 = conversion::slice_to_u16(&new_data[cursor - 2..cursor])
+        let stream_len: u16 = conversion::slice_to_u16(&new_data[cursor - LENGTH_SIZE..cursor])
             .expect("Failed to convert bytes");
         cursor -= LENGTH_SIZE;
         trace!(
